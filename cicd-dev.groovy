8ded5a5db9d4dd3eab984729c4c31742308e1987
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:37:49 -0400

    Change organization to zopencommunity

--- cicd-dev.groovy
+++ cicd-dev.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/grafanaport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/grafanaport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/grafanaport.git'), string(name: 'PORT_DESCRIPTION', value: 'The open and composable observability and data visualization platform. Visualize metrics, logs, and traces from multiple sources like Prometheus, Loki, Elasticsearch, InfluxDB, Postgres and many more.' ), string(name: 'BUILD_LINE', value: 'DEV') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/grafanaport.git'), string(name: 'PORT_DESCRIPTION', value: 'The open and composable observability and data visualization platform. Visualize metrics, logs, and traces from multiple sources like Prometheus, Loki, Elasticsearch, InfluxDB, Postgres and many more.' ), string(name: 'BUILD_LINE', value: 'DEV') ]
   }
 }
